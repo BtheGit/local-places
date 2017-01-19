fb14b26e8a08a36b75c45fbc473e2c100b118ded
BtheGit <brendancommerce@gmail.com>
2017-01-19 19:42:33 +0800

    Lightbox component added and working

--- src/components/Placescreen.js
+++ src/components/Placescreen.js
@@ -1,17 +1,24 @@
 import React, { Component } from  'react';
 import { hidePlacescreen } from '../actions/actions';
+import Lightbox from 'react-image-lightbox';
 
 class Place extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+            photoIndex: 0,
+            isOpen: false
+        };
 	}
 
 	renderPlace(place) {
+
+
 		return(
 			<div>
 				<h1>{place.title || ''}</h1>
 				<div>
-					{place.imagesArray.length > 0 ? <img src={place.imagesArray[0]} className="placescreen-img"/> : null}
+					{place.imagesArray.length > 0 ? <img src={place.imagesArray[0]} className="placescreen-img" onClick={() => this.setState({ isOpen: true })}/> : null}
 				</div>
 				<a href={place.website} target="_blank">{place.website}</a>
 				<p>{place.address || ''}</p>
@@ -26,11 +33,35 @@ class Place extends Component {
 
 
 	render() {
+
+		const {
+            photoIndex,
+            isOpen,
+        } = this.state;
+        const images = this.props.place[0].imagesArray;
 		return (
 			<div className="placescreen">
 				<a id="placescreen-btn-close" onClick={(e) => this.props.dispatch(hidePlacescreen())}>✕</a>
 				{this.renderPlace(this.props.place[0])}
+
+
+				{isOpen &&
+	                <Lightbox
+	                    mainSrc={images[photoIndex]}
+	                    nextSrc={images[(photoIndex + 1) % images.length]}
+	                    prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+
+	                    onCloseRequest={() => this.setState({ isOpen: false })}
+	                    onMovePrevRequest={() => this.setState({
+	                        photoIndex: (photoIndex + images.length - 1) % images.length,
+	                    })}
+	                    onMoveNextRequest={() => this.setState({
+	                        photoIndex: (photoIndex + 1) % images.length,
+	                    })}
+	                />
+	            }
 			</div>
+
 		)
 	}
 }
