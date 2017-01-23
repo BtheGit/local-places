2b3f06bf6ac155c47451bf6c48aefad4b5d3039e
BtheGit <brendancommerce@gmail.com>
2017-01-23 16:34:12 +0800

    MINOR CSS tweaking for shadow effect

---
+++ src/components/Formscreen.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Formscreen extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+
+		}
+	}
+
+	renderFormElement() {
+
+	}
+
+	componentDidMount() {
+
+	}
+
+	render() {
+		return (
+			<div className="formscreen-container">
+				Formscreen
+			</div>
+		)
+	}
+}
+
+export default Formscreen;
